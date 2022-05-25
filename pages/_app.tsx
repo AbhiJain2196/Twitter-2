f0c5c5475a749415dd7a559c11a5d323402c00fe
Abhishek <abhijain2196@gmail.com>
2022-05-25 14:32:22 +0530

    Initial Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { SessionProvider } from 'next-auth/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
+  return (
+    <>
+      <Head>
+        <title>Twitter 2</title>
+        <link rel="icon" href="https://links.papareact.com/drq" />
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
+  )
 }
 
 export default MyApp
